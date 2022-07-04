292d75ee6c320f0589aedac4ff04aafc0910aaa7
tod11 <119330371@umail.ucc.ie>
2022-07-04 16:08:43 +0100

    parsed param and binded to go struct

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -10,22 +11,45 @@ type HelloWorld struct {
 	Message string `json:"message"`
 }
 
+type Person struct {
+	Name string `json:"name"`
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.GET("/contact", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Contact!")
-	})
+	e.GET("/", Home)
+	e.GET("/contact", Contact)
 
-	e.GET("/contact/:name", GreetingsWithParams)
+	e.GET("/params/:data", getParams)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func GreetingsWithParams(c echo.Context) error {
-	params := c.Param("name")
-	return c.JSON(http.StatusOK, HelloWorld{
-		Message: "Hello World, my name is " + params,
-	})
+func getParams(c echo.Context) error {
+	person := Person{}
+
+	err := c.Bind(&person)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed processing request")
+	}
+
+	datatype := c.Param("data")
+
+	// if datatype == "string" {
+	// 	return c.String(http.StatusOK, fmt.Sprintf("This is the param name you sent us '%s'", person.Name))
+	// }
+
+	if datatype == "json" {
+		return c.String(http.StatusOK, fmt.Sprintf("This is the param name you sent us '%s'", person.Name))
+	}
+
+	return c.JSON(http.StatusBadRequest, fmt.Sprintf("Invalid parameter type: %v", datatype))
+
+}
+
+func Home(c echo.Context) error {
+	return c.String(http.StatusOK, "Welcome Home!")
+}
+
+func Contact(c echo.Context) error {
+	return c.String(http.StatusOK, "Contact!")
 }
